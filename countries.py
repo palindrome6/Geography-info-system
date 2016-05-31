4418f38e89d4f2497cb1e4b4e194cc683c752237
Nitin Narayan <nitinnarayan1501@gmail.com>
2016-05-31 15:43:40 +0200

    fixed ascii bug

--- countries.py
+++ countries.py
@@ -2,18 +2,15 @@ from html import XHTML
 import pycountry
 import wikipedia
 
-
+summary = []
 country_list = list(pycountry.countries)
-i = 1
 for country in country_list:
     print country.name
-    summary1 = wikipedia.summary(str(country.name))
+    summary1 = wikipedia.summary(country.name)
     summary1 = summary1.encode('utf-8')
     h = XHTML()
     h.div(summary1)
     cname = (str(country.alpha2)).lower() + ".xhtml"
     with open(str(cname), "w") as cfile:
         cfile.write(str(h))
-    # print i
-    # i += 1
-    break
+    summary1 = str(summary1)
