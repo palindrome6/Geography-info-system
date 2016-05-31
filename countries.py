4cf00a40ea9c1c74fc5fddccf5b5e184b7c5f445
Nitin Narayan <nitinnarayan1501@gmail.com>
2016-05-31 18:05:40 +0200

    Fixed country name ambiguity problem. Run separately  for Georgia and Congo.

--- countries.py
+++ countries.py
@@ -5,12 +5,16 @@ import wikipedia
 summary = []
 country_list = list(pycountry.countries)
 for country in country_list:
+    if country.name in ["Congo", "Georgia"]:
+        continue
+    else:
+        summary1 = wikipedia.summary(country.name)
     print country.name
-    summary1 = wikipedia.summary(country.name)
     summary1 = summary1.encode('utf-8')
     h = XHTML()
     h.div(summary1)
     cname = (str(country.alpha2)).lower() + ".xhtml"
-    with open(str(cname), "w") as cfile:
+    direc = "C:\Users\s158079\Downloads\Study_materials\Quartile 4\Adaptive web\Project\countries\\"
+    with open(direc + str(cname), "w") as cfile:
         cfile.write(str(h))
     summary1 = str(summary1)
