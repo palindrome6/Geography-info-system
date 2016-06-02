11ceda7caeb891e1072818bf8beb231b3b55aef2
Nitin Narayan <nitinnarayan1501@gmail.com>
2016-06-02 11:16:32 +0200

    generate xhtml with proper tags

--- extra_country.py
+++ extra_country.py
@@ -4,15 +4,12 @@ import wikipedia
 
 summary = []
 country_list = list(pycountry.countries)
-for country in country_list:
-    if country.name in ["Congo", "Georgia"]:
-        continue
-    else:
-        summary1 = wikipedia.summary(country.name)
-    print country.name
+for country in ["Georgia(country)"]:
+    summary1 = wikipedia.summary(country)
+    print country
     summary1 = summary1.encode('utf-8')
     paras = summary1.split("\n")
-    cname = (str(country.alpha2)).lower() + ".xhtml"
+    cname = ("GE").lower() + ".xhtml"
     direc = "C:\Users\s158079\Downloads\Study_materials\Quartile 4\Adaptive web\Project\countries\\"
     with open(direc + str(cname), "w") as cfile:
         cfile.write("<div>\n")
--- sub_continents.py
+++ sub_continents.py
@@ -3,13 +3,13 @@ import pycountry
 import wikipedia
 
 summary = []
-continent_names = ["Africa", "Americas", "Asia", "Europe", "Oceania"]
+continent_names = ["Australia and New Zealand", "Melanesia", "Micronesia", "Polynesia", "Earth"]
 for continent in continent_names:
     summary1 = wikipedia.summary(continent)
     print continent
     summary1 = summary1.encode('utf-8')
     paras = summary1.split("\n")
-    cname = (continent).lower() + ".xhtml"
+    cname = continent.lower() + ".xhtml"
     direc = "C:\Users\s158079\Downloads\Study_materials\Quartile 4\Adaptive web\Project\continents\\"
     with open(direc + str(cname), "w") as cfile:
         cfile.write("<div>\n")
