6f786e316297cae1e581b64a1435700e3392182a
Jeroen Noten <jeroennoten@me.com>
2016-06-07 15:36:31 +0200

    generate gam file via template

---
+++ generategam2.py
@@ -0,0 +1,36 @@
+import pycountry
+import pystache
+from slugify import slugify
+
+def get_country_title_by_code(code):
+	return pycountry.countries.get(alpha2=code).name.encode('ascii', 'ignore').decode('ascii')
+
+def map_concept(concept, level, parent):
+	title = get_country_title_by_code(concept) if level is '3' else concept
+	return {
+		'title': title,
+		'level': level,
+		'name': slugify(concept),
+		'filename': slugify(concept, separator='_'),
+		'parent': slugify(parent) if parent is not None else None
+	};
+
+with open('countrylist.txt', 'r') as country_file:
+	
+	concepts = []
+	parents = {}
+
+	for concept_data in country_file.read().splitlines():
+		(concept, level) = concept_data.split(',')
+		parents[level] = concept
+		parent_level = str(int(level) - 1)
+		parent = parents[parent_level] if parent_level in parents else None
+		concepts.append(map_concept(concept, level, parent))
+
+	with open('concepts.mustache', 'r') as template_file:
+		with open('concepts.gam', 'w') as concepts_file:
+			renderer = pystache.Renderer(escape=lambda u: u)
+			
+			concepts_file.write(renderer.render(template_file.read(), {
+				'concepts': concepts
+			}))
