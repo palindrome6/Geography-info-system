d1c70fe27836568ef3922e81230700d065f29fc3
Jeroen Noten <jeroennoten@me.com>
2016-06-09 09:31:02 +0200

    modified gam

--- generategam2.py
+++ generategam2.py
@@ -6,13 +6,15 @@ def get_country_title_by_code(code):
 	return pycountry.countries.get(alpha2=code).name.encode('ascii', 'ignore').decode('ascii')
 
 def map_concept(concept, level, parent):
-	title = get_country_title_by_code(concept) if level is '3' else concept
+	country = level is '3'
+	title = get_country_title_by_code(concept) if country else concept
 	return {
 		'title': title,
 		'level': level,
 		'name': slugify(concept),
 		'filename': slugify(concept, separator='_'),
-		'parent': slugify(parent) if parent is not None else None
+		'parent': slugify(parent) if parent is not None else None,
+		'country': country
 	};
 
 with open('countrylist.txt', 'r') as country_file:
